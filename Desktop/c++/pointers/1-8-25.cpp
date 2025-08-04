d9ba888d46370de116016123667125c659cc4849
PETERPARKERKJII <ilikeihsuraa@gmail.com>
2025-08-04 12:09:07 +0530

    code

--- Desktop/c++/pointers/1-8-25.cpp
+++ Desktop/c++/pointers/1-8-25.cpp
@@ -1,14 +1,26 @@
+// #include<iostream>
+// using namespace std;
+// struct Student{
+//     int id;
+//     double marks;
+// };
+// int main(){
+//     Student s1; // Create a Point(object)
+//     s1.id = 101;
+//     s1.marks = 93.5;
+//     cout<< "Student id: "<<s1.id<<endl;
+//     cout<< "Student marks: "<<s1.marks<<endl;
+//     return 0;
+// }
 #include<iostream>
 using namespace std;
-struct Student{
-    int id;
-    double marks;
-};
+namespace physics{
+    double gravity(){
+        return 9.8;
+    }
+}
 int main(){
-    Student s1; // Create a Point(object)
-    s1.id = 101;
-    s1.marks = 93.5;
-    cout<< "Student id: "<<s1.id<<endl;
-    cout<< "Student marks: "<<s1.marks<<endl;
+    double g= physics :: gravity();
+    cout << "Acceleration due to gravity is: " << g << " m/s²" << endl;
     return 0;
 }
