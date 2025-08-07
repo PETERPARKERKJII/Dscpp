828f7a9bef6eb7f77c88c62b0f32a712ba787964
PETERPARKERKJII <ilikeihsuraa@gmail.com>
2025-08-07 12:11:48 +0530

    code

--- Desktop/c++/pointers/7-8-25.cpp
+++ Desktop/c++/pointers/7-8-25.cpp
@@ -1,21 +1,40 @@
+// #include<iostream>
+// using namespace std;
+
+// class Demo {
+//     int x;
+
+// public:
+//     Demo(int a) {  // public constructor
+//         x = a;
+//     }
+
+//     void show() {
+//         cout << "Value of x: " << x << endl;
+//     }
+// };
+
+// int main() {
+//     Demo d1(10);  // works now
+//     d1.show();    // prints the value
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 
-class Demo {
-    int x;
+class Point{
+    private:
+     int x;
+     int y;
 
-public:
-    Demo(int a) {  // public constructor
-        x = a;
-    }
-
-    void show() {
-        cout << "Value of x: " << x << endl;
-    }
+    public:
+     Point(int a, int b):x(a),y(b){
+        cout<<"Point initialized with x = " << x<<" and y = "<<y<<endl;
+     }
 };
-
-int main() {
-    Demo d1(10);  // works now
-    d1.show();    // prints the value
+int main(){
+    Point p1(3,4);
     return 0;
 }
